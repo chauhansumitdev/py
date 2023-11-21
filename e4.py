0b48c13050247056a2aa780d23a3f0949de293dc
Sumit Chauhan <chauhansumit9009@gmail.com>
2023-11-21 16:13:13 +0530

    linearRegression

---
+++ e4.py
@@ -0,0 +1,15 @@
+# Supervised learning
+# regression
+#classification
+
+
+
+#unsupervised learning
+#clustering
+
+
+# terms
+# training set
+# x - feature
+# y - output
+# (x,y) single training example
